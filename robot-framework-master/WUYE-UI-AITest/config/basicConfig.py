b06c7633d25799a239ebcdd74d688146ca338376
lh <1185880010@qq.com>
2022-05-24 22:48:14 +0800

    第一次提交代码

---
+++ robot-framework-master/WUYE-UI-AITest/config/basicConfig.py
@@ -0,0 +1,13 @@
+# Can be used in the test data like ${MyObject()} or ${MyObject(1)}
+'''
+配置连接信息
+'''
+sqlconfig="DRIVER='{SQL Server}',SERVER='PMS-DATASERVER',DATABASE='pms_xinye_hotfix',UID='sa',PWD='Sa123456'"
+username = 'admin' # 'testing'
+password = 'Nova@2015' # '123456'
+用户名="id=UserName"
+密码="id=Password"
+登录按钮="//button/span[text()='立即登录']/.."
+# 关闭新手指引 = "//button/span[text()='关闭新手指引']/.."  # "//button[@_nk='lepl22']"
+# 我知道了 = "//button[@_nk='Jzz1k1']"
+关闭 = "xpath=//*[@id='___reactour']/div[4]/div/button"
